6df662b4fc1ce9925297a20ebd8f41e68a4dfed7
Cherrypick14 <cherrypicklet@gmail.com>
2023-01-22 23:16:43 +0300

    add routes

--- react_scart/src/App.js
+++ react_scart/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import {BrowserRouter as Router,Routes, Route } from 'react-router-dom'
+import { Navbar } from './components/navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Here is my React Shop Cart</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+           <Route path = "/" />
+           <Route path = "/cart" />
+        </Routes>
+      </Router>
 
     </div>
   );
