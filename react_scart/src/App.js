2b38b0238b5bc6f8286c3f7d7f1ecae206514f79
Cherrypick14 <cherrypicklet@gmail.com>
2023-01-24 23:11:22 +0300

    import cart and shop components

--- react_scart/src/App.js
+++ react_scart/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import {BrowserRouter as Router,Routes, Route } from 'react-router-dom'
 import { Navbar } from './components/navbar';
+import { Shop } from '../src/pages/shop';
+import { Cart } from '../src/pages/cart';
 
 function App() {
   return (
@@ -8,8 +10,8 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-           <Route path = "/" />
-           <Route path = "/cart" />
+           <Route path = "/" element={<Shop />} />
+           <Route path = "/cart" element={<Cart />} />
         </Routes>
       </Router>
 
