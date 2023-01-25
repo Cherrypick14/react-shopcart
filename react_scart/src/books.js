d657725ba98720789b2bb43d93ad795f5078b7d0
Cherrypick14 <cherrypicklet@gmail.com>
2023-01-26 00:11:04 +0300

    modified file extension

--- react_scart/src/books.js
+++ react_scart/src/books.js
@@ -1,9 +1,69 @@
-export  const BOOKS = [
+import product1 from './assets/image1.jpg';
+import product2 from './assets/image2.jpg';
+import product3 from './assets/image3.jpg';
+import product4 from './assets/image4.jpg';
+import product5 from './assets/image5.jpg';
+import product6 from './assets/image6.jpg';
+import product7 from './assets/image7.jpg';
+import product8 from './assets/image8.jpg';
+
+
+export const BOOKS = [
     {
        id:1,
        bookTitle:"Eat that Frog",
        author:'Brian Tracy',
        price:8.39,
-       bookImage:
-    }
+       bookImage:product1
+    },
+    {
+        id:2,
+        bookTitle:"Lean In",
+        author:'Sheryl Sandberg',
+        price:9.39,
+        bookImage:product2
+     },
+     {
+        id:3,
+        bookTitle:"10 Golden principles of warren buffet",
+        author:'Warren Buffet',
+        price:8.39,
+        bookImage:product3
+     },
+     {
+        id:4,
+        bookTitle:"7 years with Jim Rohn",
+        author:'Chris Widener',
+        price:8.39,
+        bookImage:product4
+     },
+     {
+        id:5,
+        bookTitle:"What I know for sure",
+        author:'Oprah Winfrey',
+        price:8.39,
+        bookImage:product5
+     },
+     {
+        id:6,
+        bookTitle:"Becoming Mitchelle Obama",
+        author:'Mitchelle Obama',
+        price:8.39,
+        bookImage:product6
+     },
+     {
+        id:7,
+        bookTitle:"We're going to need more wine",
+        author:'Gabrielle Union',
+        price:8.39,
+        bookImage:product7
+     },
+     {
+        id:8,
+        bookTitle:"The Power Playbook",
+        author:'La la Anthony',
+        price:8.39,
+        bookImage:product8
+     },
+
 ]
