fa17fc8a795c0f776f871fabca2e831d2bdd0dec
Cherrypick14 <cherrypicklet@gmail.com>
2023-01-28 22:02:09 +0300

    add book content

--- react_scart/src/pages/shop/book.jsx
+++ react_scart/src/pages/shop/book.jsx
@@ -4,11 +4,19 @@ export const Book = (props) => {
     const {id,bookTitle,author,price,bookImage} = props.data
   return (
     <div className='book'>
-        <div className="bookid">{id}</div>
-         <div className="title">{bookTitle}</div>
-         <div className="author">{author}</div>
-         <div className="price">{price}</div>
-         <div className="bookimage">{bookImage}</div>
+        <img src ={bookImage} />
+
+        <div className="description">
+          <p>
+            <b>{bookTitle}</b>
+            </p>
+          <p>
+            <i>{author}</i>
+            </p>
+          <p>
+            <b>${price}</b>
+            </p>
+        </div>
     </div>
   )
 }
