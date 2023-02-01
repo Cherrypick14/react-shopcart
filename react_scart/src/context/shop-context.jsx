68db7f4121843a26f29d8a6ce780b09dab2fce43
Cherrypick14 <cherrypicklet@gmail.com>
2023-02-01 23:17:40 +0300

    add cart function

--- react_scart/src/context/shop-context.jsx
+++ react_scart/src/context/shop-context.jsx
@@ -1,9 +1,23 @@
+import { Books } from 'phosphor-react';
 import React,{createContext, useState} from 'react'
+import { BOOKS } from '../../src/books';
 
 export const ShopContext = createContext(null);
 
+// getting initial cart item
+// add more products to the cart
+// handle the changes automatically
+
+const getDefaultCart =() =>{
+    let cart ={}
+    for(let i = 1; i< BOOKS.length + 1; i++){
+        cart[i] = 0
+    }
+    return cart ;
+};
+
 export const ShopContextProvider = (props) => {
-    const[cartItems, setCartItems] = useState({})
+    const[cartItems, setCartItems] = useState(getDefaultCart())
 
   return (
 
