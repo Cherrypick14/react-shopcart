b65e15c44a7b62a613ff34ea86bc8ef3c58d7459
Cherrypick14 <cherrypicklet@gmail.com>
2023-02-08 00:17:25 +0300

    import cart item

--- react_scart/src/pages/cart/cart.jsx
+++ react_scart/src/pages/cart/cart.jsx
@@ -1,6 +1,7 @@
 import React,{useContext}from 'react'
 import {BOOKS} from '../../books'
 import { ShopContext } from '../../context/shop-context'
+import { CartItem } from './cart-item'
 
 export const Cart = () => {
    const {cartItems} = useContext(ShopContext)
@@ -12,8 +13,10 @@ export const Cart = () => {
             <h1>My Cart Items</h1>
           </div>
            <div className="cartitems">
-                {BOOKS.map((book)=>
-                 { })}
+                { BOOKS.map((book)=>
+                 {if (cartItems[book.id] !== 0){
+                        return <CartItem data ={book}/>
+                 }})}
            </div>
     </div>
   )
