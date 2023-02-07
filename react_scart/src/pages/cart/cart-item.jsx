8972dcade5c53e540248f6417222f8fbaee0b44b
Cherrypick14 <cherrypicklet@gmail.com>
2023-02-08 00:23:48 +0300

    add cartitem contents

--- react_scart/src/pages/cart/cart-item.jsx
+++ react_scart/src/pages/cart/cart-item.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
 export const CartItem= (props) => {
+
+  const {id,bookTitle,author,price,bookImage} = props.data
   return (
     <div className='cartitem'>
-      cartItem
+
+      <img src={bookImage}/>
+
+      <div className='description'>
+        <p>
+          <b>{bookTitle}</b>
+          </p>
+        <p>
+          <b>{author}</b>
+          </p>
+        <p>${price}</p>
+      </div>
+
     </div>
   )
 }
