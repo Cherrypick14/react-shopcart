669533017ca335b2f6a162806f315a9c7c53bf1b
Cherrypick14 <cherrypicklet@gmail.com>
2023-02-07 23:54:15 +0300

    map through the books

--- react_scart/src/pages/cart/cart.jsx
+++ react_scart/src/pages/cart/cart.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import {BOOKS} from '../../books'
 
 export const Cart = () => {
   return (
-    
+
     <div className='cart'>
-          
+
+          <div>
+            <h1>My Cart Items</h1>
+          </div>
+           <div className="cartitems">
+                {BOOKS.map((book)=>
+                 {})}
+           </div>
     </div>
   )
 }
